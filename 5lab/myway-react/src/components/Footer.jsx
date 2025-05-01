48a05e7a73a8c7db27edd8eb491a9b9ab36aeee6
nastly29 <anastasiadavid312@gmail.com>
2025-05-02 00:20:51 +0300

    Delete 5lab directory

--- 5lab/myway-react/src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export default function Footer(){
-    return (
-        <footer>
-          <p>Контактна інформація: myway.info@gmail.com</p>
-          <p>&copy; 2025 MyWay. Всі права захищені</p>
-        </footer>
-    );
-}
